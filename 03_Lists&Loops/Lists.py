515479feaae5b358c490916ef11c92994699bece
uwn-online <nduugochukwuwalters@gmail.com>
2025-10-19 23:18:03 +0100

    Added List practice script

---
+++ 03_Lists&Loops/Lists.py
@@ -0,0 +1,5 @@
+data = [53, 44, 45, 21, 33, 56, 67, 90]
+print(data[-4])
+
+for num in data:
+    print(num, end= ' ')
---
+++ 03_Lists&Loops/iteration.py
@@ -0,0 +1,30 @@
+# Loops
+# for i in range(10): 
+#     print(i)
+
+# for i in range(10):
+#     print(i, end =' ') #Prints on the same line
+
+# for i in range(1, 11): #Start and end number are included.
+#     print(i, end = ' ')
+
+# Steps function 
+# for i in range(0, 101, 10): #Includes step function/increment value.
+#     print(i, end =' ')
+
+# for i in range(101, 0, -4):
+#     print(i, end =' ')
+
+
+word = 'Python'
+for char in word:
+    print(char)
+
+# Print all even numbers between 1 and 20
+
+for i in range(2, 20, 2):
+    print(i)
+
+x = 5
+x = x+1
+print(x)
