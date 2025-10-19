4f6d34457e0e030952ae0c60d064614779c05742
uwn-online <nduugochukwuwalters@gmail.com>
2025-10-19 10:39:49 +0100

    Added conditionals folder

---
+++ 02_Conditionals/Logical_operators.py
@@ -0,0 +1,19 @@
+
+
+var_1 = 7
+var_2 = 5
+
+print('var_1 =', var_1, 'var_2 =', var_2)
+print('Checking equality with \'==\':', var_1 == var_2)
+print('Checking not equal with \'!=\':', var_1 != var_2)
+print('Checking less than or equal with \'<=\':', var_1 <= var_2)
+print('Checking greater than or equal with \'>=\':', var_1 >= var_2)
+
+print(type(True))
+
+x = 4
+y = 10
+if x > y:
+    print("x is greater than y")
+else:
+    print("x is not greater than y")
