e8be68d97fc979e4c3347ccb3ae597e49fe584e3
uwn-online <nduugochukwuwalters@gmail.com>
2025-10-20 12:52:40 +0100

    Updated script

--- 03_Lists&Loops/Lists.py
+++ 03_Lists&Loops/Lists.py
@@ -1,5 +1,10 @@
 data = [53, 44, 45, 21, 33, 56, 67, 90]
-print(data[-4])
+# print(data[-4])
 
+# for num in data:
+#     print(num, end= ' ')
+
+total = 0
 for num in data:
-    print(num, end= ' ')
+    total = total + num
+print('The sum total is', total)
