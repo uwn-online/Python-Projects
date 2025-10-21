a99e1fe5ab38bf9c2f795e94a0238d7dd1a1b837
uwn-online <nduugochukwuwalters@gmail.com>
2025-10-21 11:35:43 +0100

    Modified scripts

---
+++ 04_datatypes/split.py
@@ -0,0 +1,6 @@
+text = "   Learning Python   is fun and   engaging! Let's    split these words properly.   "
+
+# Use split() to separate text into individual words
+words = text.split() #Assign variable to store the split
+
+print(words)
